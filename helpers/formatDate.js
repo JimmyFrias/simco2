9eec73c61fb554dab3ea3cbca8187c1186b1bcc9
neiderbtc <neiderbtc@gmail.com>
2021-10-19 00:53:41 -0500

    add endDate and startDate

--- helpers/formatDate.js
+++ helpers/formatDate.js
@@ -3,3 +3,7 @@ import moment from "moment";
 export const formatDate = (date) => {
     return moment(date).format('DD/MM/YYYY');
 };
+
+export const endDate = moment().format('DD/MM/YYYY');
+
+export const startDate = moment().subtract(1, 'month').format('DD/MM/YYYY');
