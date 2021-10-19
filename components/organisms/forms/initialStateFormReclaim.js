6a6ca2e7c22124e68ec6f45d7a55a3c8de211c5e
neiderbtc <neiderbtc@gmail.com>
2021-10-19 00:51:50 -0500

    extract initial state from reclaims

---
+++ components/organisms/forms/initialStateFormReclaim.js
@@ -0,0 +1,13 @@
+export const initialStateFormReclaim = {
+    folio: false,
+    distributor: false,
+    fechaSuceso: false,
+    plaza: '',
+    idplaza: '', idproducto: '',
+    canaltransaccion: '',
+    origenreclamacion: '',
+    motivoreclamacion: '',
+    endFiles: [],
+    claveempresaria: false,
+    importereclamado: ''
+};
